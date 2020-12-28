c8490dd7836e00a22532c5384223952b0eefaba6
雾凇 <653040200@qq.com>
2020-12-28 13:46:45 +0800

    demo

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,23 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
-  
+  { path: '/', redirect: '/login' },
+  {
+    path:'/login',
+    name:'login',
+    component: () => import('@/views/login'),
+    meta:{
+      title:'登录'
+    }
+  },
+  {
+    path:'/index',
+    name:'index',
+    component: () => import('@/views/index'),
+    meta:{
+      title:'物流管理系统'
+    }
+  },
 ]
 
 const router = new VueRouter({
---
+++ src/views/login.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="flex">
+    <div class="login-form">
+      <el-form>
+        <el-form-item label="用户名">
+          <el-input></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input></el-input>
+        </el-form-item>
+        <el-form-item class="flex-between">
+          <el-button type="primary">登录</el-button>
+          <el-button type="info">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+<style>
+.flex {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 100px;
+}
+.login-form {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid;
+  border-radius: 10px;
+  height: 450px;
+  width: 350px;
+}
+</style>
